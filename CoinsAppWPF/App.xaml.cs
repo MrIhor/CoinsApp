c27e433e39c2071005d0b92c80b83783c61624bd
ihormolchan <molchanigor54@gmail.com>
2023-05-24 19:54:48 +0300

    create project

---
+++ CoinsAppWPF/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CoinsAppWPF.ViewModels;
+using CoinsAppWPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoinsAppWPF
+{
+    public partial class App : Application
+    {
+        public static NavigatorService<Page> Navigator = new NavigatorService<Page>();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+
+            base.OnStartup(e);
+        }
+    }
+}
