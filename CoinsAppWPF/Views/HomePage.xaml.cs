0657cb271703d6fa6ea1e7f67f67b08ec5246e25
ihormolchan <molchanigor54@gmail.com>
2023-05-30 14:59:56 +0300

    add event for clear search text and update work with view model

--- CoinsAppWPF/Views/HomePage.xaml.cs
+++ CoinsAppWPF/Views/HomePage.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,12 +20,10 @@ namespace CoinsAppWPF.Views
 {
     public partial class HomePage : UserControl
     {
-        private HomePageViewModel _homeViewModel;
         public HomePage()
         {
             InitializeComponent();
             DataContext = new HomePageViewModel();
-            _homeViewModel = new HomePageViewModel();
         }
 
         private void ContentPresenter_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -33,16 +32,23 @@ namespace CoinsAppWPF.Views
             var coin = element?.DataContext as CoinSearch;
             App.Navigator.Navigate(new CoinDetailPage(coin));
 
+            var _homeViewModel = DataContext as HomePageViewModel;
+
             if (_homeViewModel != null )
             {
-                _homeViewModel.SearchText = "";
                 _homeViewModel.SearchResults = null;
             }
         }
 
-        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            _homeViewModel.IsDropdownOpen = false;
+            var _homeViewModel = DataContext as HomePageViewModel;
+
+            if (_homeViewModel != null)
+            {
+                _homeViewModel.SearchText = "";
+                _homeViewModel.SearchResults = null;
+            }
         }
     }
 }
