e4a9c50c8077ec56e2e472350f99316538e15cb5
ihormolchan <molchanigor54@gmail.com>
2023-06-04 17:26:09 +0300

    create currency service interface

---
+++ CoinsAppWPF/Services/ICurrencyService.cs
@@ -0,0 +1,16 @@
+﻿using CoinsAppWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoinsAppWPF.Services
+{
+    public interface ICurrencyService
+    {
+        Task<List<string>> GetSymbols(CancellationToken cancellationToken);
+        Task<decimal> Convert(decimal amount, string to, string from, CancellationToken cancellationToken);  
+    }
+}
